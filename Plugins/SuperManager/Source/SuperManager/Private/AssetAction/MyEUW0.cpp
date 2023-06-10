3328ab915ad735aa518f5db5eed5a85e266425e2
pampa <pampa@126.com>
2023-06-10 16:35:45 +0800

    if meet unknowen eror like : UMovieSceneEventTrack::GetPrivateStaticClass(void)" (__imp_?GetPrivateStaticClass@UMovieSceneEventTrack@@CAPEAVUClass@@XZ) referenced in function "public: void __cdecl UMyEUW0::TestFun(void)" (?TestFun@UMyEUW0@@QEAAXXZ)                              E:\dev\ue\hello\MyProject1\Plugins\SuperManager\Binaries\Win64\UnrealEditor-SuperManager.patch_6.exe : fatal error LNK1120: 1 unresolved external , clean and rebuild from rider may works

--- Plugins/SuperManager/Source/SuperManager/Private/AssetAction/MyEUW0.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/AssetAction/MyEUW0.cpp
@@ -2,7 +2,10 @@
 
 
 #include "AssetAction/MyEUW0.h"
-
+#include "EditorAssetLibrary.h"
+#include "Tracks/MovieSceneEventTrack.h"
+#include "MovieSceneTrack.h"
+#include "Sections/MovieSceneEventSection.h"
 #include <LevelSequenceActor.h>
 #include <LevelSequencePlayer.h>
 #include <MovieSceneSequencePlayer.h>
@@ -19,6 +22,31 @@ void UMyEUW0::TestFun()
 
 	UE_LOG(LogTemp, Display, TEXT("sequences.Num() = %d"), sequences.Num());
 
+	if (sequences.Num() == 0)
+	{
+		return;
+	}
+	
+	UE_LOG(LogTemp, Display, TEXT("sequnces[0] = %s"), *sequences[0]->GetName());
+	
+
+	UEditorAssetLibrary::CheckoutAsset("/Game/NewLevelSequence");
+	UObject* mySequenceObject = UEditorAssetLibrary::LoadAsset("/Game/NewLevelSequence");
+	UMovieSceneSequence* myMovieSceneSequence = Cast<UMovieSceneSequence>(mySequenceObject);
+	UMovieScene* myMovieScene = myMovieSceneSequence->GetMovieScene();
+
+	// Fetch blueprint and add as spawnable in sequence
+	UObject* myBlueprintObject = UEditorAssetLibrary::LoadAsset("/Game/Test/BP_EventCubo0Pawn");
+	FGuid mySpanwableGuid = myMovieSceneSequence->CreateSpawnable(myBlueprintObject);
+	
+	// Add an event track to the spawnable
+	UMovieSceneTrack* myTrack = myMovieScene->AddTrack(UMovieSceneEventTrack::StaticClass(), mySpanwableGuid);
+	
+	// Add a section to the event track
+	UMovieSceneSection* mySection = myTrack->CreateNewSection();
+	myTrack->AddSection(*mySection);
+	UMovieSceneEventSection* myEventSection = Cast<UMovieSceneEventSection>(mySection);
+	
 	
 }
 
