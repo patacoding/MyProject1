8e06372cc2ecef95cbc5c78a304ffb05b0781918
pampa <pampa@126.com>
2023-06-19 10:08:18 +0800

    add notifyinfo dialog fuction

--- Plugins/MyPugin0/Source/MyPugin0/Public/AssetAction/DebugHeader.h
+++ Plugins/MyPugin0/Source/MyPugin0/Public/AssetAction/DebugHeader.h
@@ -1,4 +1,8 @@
 ﻿#pragma once
+#include "Misc/MessageDialog.h"
+#include "Widgets/Notifications/SNotificationBackground.h"
+#include "Framework/Notifications/NotificationManager.h"
+#include "Widgets/Notifications/SNotificationList.h"
 
 void MyPrint(const FString& Message, const FColor& Color)
 {
@@ -12,4 +16,14 @@ void MyPrint(const FString& Message, const FColor& Color)
 void MyPrintLog(const FString& Message)
 {
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+}
+
+
+void MyNotifyInfo(const FString& Message)
+{
+	FNotificationInfo NotifyInfo(FText::FromString(Message));
+	NotifyInfo.bUseLargeFont = true;
+	NotifyInfo.FadeInDuration = 8.0f;
+	FSlateNotificationManager::Get().AddNotification(NotifyInfo);
+	
 }
