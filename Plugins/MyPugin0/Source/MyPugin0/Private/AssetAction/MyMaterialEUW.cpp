4988e1ae2a8bb028e6d83392c2b3f31fbedacad1
Wang JunKai <patacoding@gmail.com>
2023-06-21 20:08:34 +0800

    backup

--- Plugins/MyPugin0/Source/MyPugin0/Private/AssetAction/MyMaterialEUW.cpp
+++ Plugins/MyPugin0/Source/MyPugin0/Private/AssetAction/MyMaterialEUW.cpp
@@ -141,12 +141,12 @@ void UMyMaterialEUW::TestKeyFrame()
 	FGuid Guid = FoundActors[77]->GetActorGuid();
 	UE_LOG(LogTemp, Warning, TEXT("Guid %s"), *Guid.ToString());
 
-	// Fetch blueprint and add as spawnable in sequence
-	UObject* myBlueprintObject = UEditorAssetLibrary::LoadAsset("/Game/BP_myactor1");
-	FGuid mySpanwableGuid = myMovieSceneSequence->CreateSpawnable(myBlueprintObject);
-	
-	// try to add event track for specific actor
-	UMovieSceneTrack* myTrack = myMovieScene->AddTrack(UMovieSceneEventTrack::StaticClass(), mySpanwableGuid);
+	// // Fetch blueprint and add as spawnable in sequence
+	// UObject* myBlueprintObject = UEditorAssetLibrary::LoadAsset("/Game/BP_myactor1");
+	// FGuid mySpanwableGuid = myMovieSceneSequence->CreateSpawnable(myBlueprintObject);
+	//
+	// // try to add event track for specific actor
+	// UMovieSceneTrack* myTrack = myMovieScene->AddTrack(UMovieSceneEventTrack::StaticClass(), mySpanwableGuid);
 
 	// // UMovieSceneTrack* MovieSceneTrack = myMovieScene->FindTrack(UMovieScene3DTransformTrack::StaticClass(), Guid);
 	static const FName s_transformTrackName(TEXT("Transform"));
